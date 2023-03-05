a220c9fdf6f3201bc778f7eb0a6276b2b22cbcd6
Bernold Kraft <bernold.kraft@gmail.com>
2023-03-05 19:16:51 +0100

    Updating assimp to latest master

---
+++ TRexDynamo/Geom/GlobalReferenceAxis.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.DesignScript.Runtime;
+
+namespace TRex.Geom
+{
+    /// <summary>
+    /// Global reference axis identifiers.
+    /// </summary>
+    [IsVisibleInDynamoLibrary(false)]
+    public enum GlobalReferenceAxis
+    {
+        NegativeX = -1,
+        NegativeY = -2,
+        NegativeZ = -3,
+
+        PositiveX = 1,
+        PositiveY = 2,
+        PositiveZ = 3    
+    }
+
+    public static class GlobalReferenceAxisExtensions
+    {
+        [IsVisibleInDynamoLibrary(false)]
+        public static GlobalReferenceAxis Invert(this GlobalReferenceAxis axis)
+        {
+            return (GlobalReferenceAxis)((int)axis * -1);
+        }
+    }
+}
